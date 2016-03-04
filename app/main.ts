7cfb72b1fd07553fff19112291b872195eb2c064
Samir Damle <damlemail@gmail.com>
2016-03-03 22:50:11 -0500

    Commit from Mac Mini

--- app/main.ts
+++ app/main.ts
@@ -2,7 +2,4 @@ import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './app.component'
 
 bootstrap(AppComponent);
-<<<<<<< HEAD
 
-=======
->>>>>>> 0bb9741d9df0911b4433d984d67015c8d7e2c2b6
